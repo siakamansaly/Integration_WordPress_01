7ee140baaa9c4e243cf1f11a86f8f624dfcd10d8
Siaka MANSALY <siaka.mansaly@gmail.com>
2021-11-04 22:55:17 +0100

    Dépot 2

---
+++ wp-content/plugins/wp-seopress/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '5.2.x-dev',
+        'version' => '5.2.9999999.9999999-dev',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '89ee7633cb80c8acc3f57430e25ede492f4c5fbb',
+        'name' => 'wp-seopress/wp-seopress',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'wp-seopress/wp-seopress' => array(
+            'pretty_version' => '5.2.x-dev',
+            'version' => '5.2.9999999.9999999-dev',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '89ee7633cb80c8acc3f57430e25ede492f4c5fbb',
+            'dev_requirement' => false,
+        ),
+    ),
+);
